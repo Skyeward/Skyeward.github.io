4846b9c839d77e99bb8567f3d3e8faa8c0deb7c6
JaneBooth <janekbooth@gmail.com>
2024-03-02 15:22:48 +0000

    can uncheck boxes

--- src/app/components/GenerationInputComponent.tsx
+++ src/app/components/GenerationInputComponent.tsx
@@ -1,19 +1,38 @@
+import GenerationCheckbox from "./GenerationCheckbox";
+import { useState } from "react";
+
 const GenerationInputComponent = () => {
     const generations: number[] = [];
+    const includeArray: boolean[] = [];
     for (let i = 1; i < 10; i++) {
         generations.push(i);
+        includeArray.push(true);
     };
+    const [generationsToInclude, setGenerationsToInclude] = useState(includeArray)
+
+    const updateGenerationState = (generation: number, include: boolean) => 
+    {
+        const newIncludeArray = [...generationsToInclude];
+        newIncludeArray[generation -1] = include;
+        setGenerationsToInclude(newIncludeArray);
+    }
+
+    const generateNewPuzzle = () => {
+        console.log("Generating new puzzle and including generations: ");
+        console.log(generationsToInclude);
+    }
+
     return (
         <>
             <h3>Which generations would you like to include?</h3>
-            <div style={{display: "flex", width: "400px", flexDirection: "row", flexWrap: "wrap"}}>
+            <div style={{ display: "flex", width: "400px", flexDirection: "row", flexWrap: "wrap" }}>
                 {generations.map(gen => {
-                    return (<div key={gen} style={{ display: "flex", flexDirection: "row", minWidth: "125px", height: "50px", alignItems: "center"}}>
-                        <input type="checkbox" id={`gen-${gen}`} name={`gen-${gen}`} value={gen} checked={true} />
-                        <label htmlFor={`gen-${gen}`}>{`Gen ${gen}`}</label>
+                    return (<div key={gen}>
+                        <GenerationCheckbox generation={gen} setGeneration={updateGenerationState} />
                     </div>);
                 })}
             </div>
+            <button onClick={generateNewPuzzle}>New puzzle!</button>
         </>
     );
 }
